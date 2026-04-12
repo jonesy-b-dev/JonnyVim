21042a5b7fc3ad3ea994d39c9cad4d2b5422d984
Jonas de Bruin <jonasdebruin@proton.me>
2026-04-12 13:51:42 +0200

    Add nvim-dap and debug settings for c/c++

--- lua/jonesyConf/after/remaps.lua
+++ lua/jonesyConf/after/remaps.lua
@@ -4,8 +4,22 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fj', function()
-    require('telescope.builtin').find_files({
-        hidden = true,
-        no_ignore = true,
-    })
+	require('telescope.builtin').find_files({
+		hidden = true,
+		no_ignore = true,
+	})
 end)
+
+--nvim dap
+local dap = require('dap')
+vim.keymap.set('n', '<F9>', dap.toggle_breakpoint)
+vim.keymap.set('n', '<F5>', dap.continue)
+vim.keymap.set('n', '<F4>', dap.repl.open, { desc = "Inspect" })
+vim.keymap.set('n', '<S-F5>', dap.run_last)
+vim.keymap.set('n', '<F10>', dap.step_over)
+vim.keymap.set('n', '<F11>', dap.step_into)
+vim.keymap.set('n', '<S-F11>', dap.step_out)
+vim.keymap.set('n', '<C-F5>', dap.terminate)
+--dapui
+local dapui = require('dapui')
+vim.keymap.set('n', '<leader>du', dapui.toggle)
---
+++ lua/jonesyConf/plugins/after/mason-nvim-dap-setup.lua
@@ -0,0 +1,2 @@
+-- Dependency of nvim-dap, but needs to be setup AFTER mason, thats why its here
+require("mason-nvim-dap").setup()
