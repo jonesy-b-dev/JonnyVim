9fc28307c20dd3f24eca471b953cc0f28afcf994
Jonas de Bruin <jonasdebruin@proton.me>
2026-04-13 20:26:38 +0200

    Add debugging for dotnet project with easy-dotnet

--- lua/JonnyVim/plugins/easy-dotnet.lua
+++ lua/JonnyVim/plugins/easy-dotnet.lua
@@ -1,6 +1,6 @@
 return {
 	"GustavEikaas/easy-dotnet.nvim",
-	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+	dependencies = { "nvim-lua/plenary.nvim", "mfussenegger/nvim-dap", "nvim-telescope/telescope.nvim" },
 	config = function()
 		require("easy-dotnet").setup({
 			terminal = function(path, action, args)
