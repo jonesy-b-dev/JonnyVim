aec1cb80e4b06e5b6410899a4fd4a938d0230f02
Jonas de Bruin <jonasdebruin@proton.me>
2026-04-13 19:55:26 +0200

    Fix center screen on Ctrl+u

--- lua/JonnyVim/core/remaps.lua
+++ lua/JonnyVim/core/remaps.lua
@@ -5,7 +5,7 @@ vim.keymap.set("n", "<leader>-", "<CMD>Oil<CR>", { desc = "Open parent directory
 
 -- Extent some default keybinds to center afer running command
 vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-e>', '<C-e>zz', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'n', 'nzzzv', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'N', 'Nzzzv', { noremap = true, silent = true })
 
