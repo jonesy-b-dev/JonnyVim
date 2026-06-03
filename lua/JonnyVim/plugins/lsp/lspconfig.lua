66330280674cbae8eb3cb041cd4648e463de130a
Jonas de Bruin <jonasdebruin@proton.me>
2026-06-03 12:36:26 +0200

    Clean up lspconfig shortcuts and fix lsprestart keybind

--- lua/JonnyVim/plugins/lsp/lspconfig.lua
+++ lua/JonnyVim/plugins/lsp/lspconfig.lua
@@ -14,28 +14,14 @@ return {
 
 			-- Set keybinds
 
-			--Show LSP references is in trouble.nvim!
+			-- Show LSP references is in trouble.nvim!
+			-- Default lsp shortcuts are used, see https://neovim.io/doc/user/lsp/#_defaults
 
 			opts.desc = "Go to declaration"
-			keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-			-- These 2 seem to work the oppisite way smh up and down
+			keymap.set("n", "grd", vim.lsp.buf.declaration, opts) -- go to declaration
+
 			opts.desc = "Show LSP definitions"
 			keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-
-			opts.desc = "Show LSP implementations"
-			keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
-
-			opts.desc = "Show LSP type definitions"
-			keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-
-			opts.desc = "See available code actions"
-			keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-			opts.desc = "Smart rename"
-			keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-
-			opts.desc = "Show buffer diagnostics"
-			keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show diagnostics for file
 
 			opts.desc = "Show line diagnostics"
 			keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
@@ -46,11 +32,8 @@ return {
 			opts.desc = "Go to next diagnostic"
 			keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
-			opts.desc = "Show documentation for what is under cursor"
-			keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
 			opts.desc = "Restart LSP"
-			keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+			keymap.set("n", "<leader>rs", "<cmd>lsp restart<CR>", opts) -- mapping to restart lsp if necessary
 
 			-- Format on save
 			vim.api.nvim_create_autocmd("BufWritePre", {
